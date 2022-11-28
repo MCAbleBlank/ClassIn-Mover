114c32bee36aa2f1b480c4e85c936d43798cbe7d
CarlGao4 <gaoweiqicarl1@163.com>
2022-11-28 21:59:42 +0800

    Fix Chinese translation cause parsing hwnd error (2.1.0.1)

--- Classin_Mover.py
+++ Classin_Mover.py
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
 
-__version__ = "2.1.0"
+__version__ = "2.1.0.1"
 
 import tkinter
 import tkinter.ttk
