e4b1e76c322840958378dddf8d277a3a804c2825
CarlGao4 <gaoweiqicarl1@163.com>
2022-11-30 20:18:00 +0800

    Add comments

--- Classin_Mover.py
+++ Classin_Mover.py
@@ -29,6 +29,7 @@ import os
 import pathlib
 import pickle
 import platform
+import re
 import struct
 import sys
 import threading
@@ -37,6 +38,7 @@ import tkinter
 import tkinter.filedialog
 import tkinter.messagebox
 import tkinter.ttk
+import traceback
 import urllib
 import urllib.request
 import webbrowser
@@ -47,17 +49,19 @@ import psutil
 
 import shlex4all
 
-ClassInHwnd = []
-ClassInTitle = []
-ClassInPID = []
-run = True
-NoAdmin = False
-downloading = False
+ClassInHwnd = []  # ClassIn classroom window handles
+ClassInTitle = []  # ClassIn classroom window titles
+ClassInPID = []  # ClassIn program PIDs
+run = True  # Signal for stopping threads
+NoAdmin = False  # Stores whether the program has Administrator's privilege
+downloading = False  # To avoid downloading the new version multiple times
+# Floating icon
 icon = "c-rllZA=q)9LG^vra)9$9)zMi$wMi9rnIz_KG0Iy0s<wnk%@@wY88-y7~E7~iwFv)NUPDPyx7=YxoM0L!%V<;>I`Itn8mm#V=s)^1mnzNWbC^;X>O&ID|2&M{N`w`$Mt@n`#t@Ccddc0dR73TE=IAjfTyjkH`Z>mS&M6$Z1y^5SBTDDO&rLx*I1psPIjAfi!=3rGqr2Q`}KCGA>gIlJMGS{;O)C>>l>_%jrQGjPIEw0*NReu)@c@)1?|ovZ2;`ezS-VvuXg))<PS_?VWC2yP^nZoIXOzD68mzwTq>1jW@ct&WQar}p-{-@^Lad8dU|?VT3TvqYD!8<Qc_Z4Vq$!Jd|X`Ih7B9muV2q%v7(}))~#C?85tQC7RF#Of`WpWOr{@5v$NUR+4=eTX0zF7G-4+&FHf)6=jP^WwOX}WO(m4cWFVACBx11`kOF}Kgj_C{!{JaNH*VaRkdOe#*w|P=f-Qi&aeB^yU4LQVuK6&@$;qV000XCyFflPP(b3Us*RG}IqS;xJ#AGrp1OxApFbE&jGYFp>hRtS^Fr;TZU`Ww&5#gMCLQ4pfM=p7UiG*Rzi9V0PU|19eS?%E&Hw@J??!@+hq0fVw(i5V>AVz@6%F3GW8ETjo@(iq#F<Jr-tb;_S(`hsszyOc*%zPN+@S<UYbG*XP=YcD<gci*iub%N*O)PJs$ph<ZLVoobkDT!lCVzQg+M_Kmt?9W-o3(&w@}R3I3H|-1=L3d54@`r^-Sm9qjMt_ILs+LmLqi$fgoVss9yAt|5(xtmn4>O^_=Hcqh6(=)2?<%VW)0Tj^78UBLKcgqw6wIOq@=jGxU#Ym9(E(q<bfY#ga8IdO;1lhdGh3ELfF4}@gm{{(;f*7D}d?i>+>;RjE#-q9Cr|D^57Q8xdP5XXb`yhS4IQJ8tsM&4-d!Z^XJcfzzhrwEL*k=vxmR&LWW2PkvW4T0h`rott135k=I89N1gx!!wfv;Vg2dTr@zi=W@hHnrAr<#^m*VDF(G#XBU{MwL72cWF)=|J#S8%d;{+;6cX#*l<;!7fIYp~hty*$_M(v0Qc`a=e5k{BE$w{0!Iy#Cxr^485Hdy!Y11a~I;V%zNd+jssYC=)3UcI`%zaQ?R!l0~R;_W#_L_|<wFtlQ$BGKf5X>u_3(e%(>07iv*`0(L{3l~O4MuvxnuU)%#_wHS+I6xvuqHqhGbX-%21r}dPB$_<%1DDW3O%E5u0O0%4p}D!4ENz@jw6Da%O%)2Ik;CD+k!bS3G)R1m5V&i!xP?d_Ja~ZIBNG_HPvYW}6iU-CkEie5yU*dt;V4`LNHlp6lr%y<COvfiz`($onwl$Dt{^ny<Ku&agD#f~S%>4OK{97zVpz#398K<~XU~3nNjotq$;{;o#Pt1xh@=woJ?Y^FQD`eyu6&CSb`T&mMt2?I1U!dv`HItL`Y&G|y7$BVv9a3&16SKSdKKAuxHFnOFr7k({-o!vuA`!fB+==ODph`Rim0va$U7BQp;)QWm<kFuq1I^fz-|&D`jg&Y>lz6ho66>jGB9#yM@B}#v`8#D(&e=8Y{FoLuQ-^MrHGABqr(H!An|L`qr&|8tQ?<YG;gk~sxz63!o!hGOg>L|l;Dxe75I*WQkkl8Z=2p=S(FD?P9Q{o(sR?|7jbb(Sqj687c=`lZrAHgs8dv{P$W6jer#KHom!(MYB!xPkbT~N{?486v$FGj^N=0GuSw6%!)9~y^2<=#xXu?}o-eUfZr!#ETdAe;tFJHj96wc7UcIHtzGbVupy=Iiu8!ce>({?E7|VR{puNNrq7O626UF6aH0(J@miCPsx4Ms=aJtU+y3TfWpBNpzJv20YwA<x4e%j$U)zRtr?)Lc1%!|n%e=4<XJ=gb{kNP6#KspKR;U83GTs*s~x)#@I+S?{qXmeEB9HmAimbbKZR=j6ZYxGK`My1lGurtn_>wom<kuI;arM2Dn`wh&Te!)=l8I3C;F|DqC--nH@sOt!{r0_6-Ky<jX$F`#p(_J`m1o_<2*;`mt(e}yVMN>K`C<q<cUmhwFUm%r8btZEGOf#7bK3~}0;Q)^U^~GdjIYx%-4P_l2$7sF+!b*Y8AF?C`2M044p;#<nS0uvILAF3BJ=oHLc&XGnsZ1f2%KUg=_v+psI-R|0ZP5oins(JT4-5{S>+9cDyRU5X_J+p&uj|#Q!*SBSa}PoHJN~8~Kem)rIeJ{L@onu#`#;6XH8nMbDHm(~;tBlEZ~G4%vh8Rvn#)^S5B*2pY4;pIdGqG2H}CfUf1A@<&HDox^xKy"
 lang = "en-us"
-lang_data = {}
+lang_data = {}  # Language dicts
+user32 = ctypes.windll.user32
 
-
+# Check whether the program has Administrator's privilege
 def has_admin():
     try:
         # only windows users with admin privileges can read the C:\windows\temp
@@ -68,13 +72,14 @@ def has_admin():
         return os.environ["USERNAME"], True
 
 
+# If doesn't has Administrator's privilege, require it
 if __name__ == "__main__" and not has_admin()[1]:
     p = psutil.Process().cmdline()
     res = ctypes.windll.shell32.ShellExecuteW(
         0,
-        ctypes.create_unicode_buffer("runas"),
-        ctypes.create_unicode_buffer(p[0]),
-        ctypes.create_unicode_buffer(shlex4all.join(p[1:])),
+        ctypes.create_unicode_buffer("runas"),  # Administrator's privilege
+        ctypes.create_unicode_buffer(p[0]),  # Program
+        ctypes.create_unicode_buffer(shlex4all.join(p[1:])),  # Parameters
         0,
         5,
     )
@@ -82,20 +87,22 @@ if __name__ == "__main__" and not has_admin()[1]:
         raise SystemExit
     NoAdmin = True
 
-
+# Get text of currently chosen language
 def GetText(t):
     global lang
     if lang == "en-us":
-        return t
+        return t  # It's already fallback language
     try:
-        return lang_data[lang][t]
+        return lang_data[lang][t]  # If lang_data stored this language
     except:
-        return t
+        return t  # Use fallback
 
 
 def SetLang(targetlang):
     global lang
-    lang = targetlang
+    lang = targetlang  # Change global lang variable
+
+    # Refresh right-click menu
     im.delete(1, tkinter.END)
     im.add_checkbutton(label=GetText("Auto patch new window"), variable=DoAutoPatch)
     im.add_command(
@@ -105,6 +112,7 @@ def SetLang(targetlang):
     im.add_command(label=GetText("Check updates"), command=startCheckUpdate)
     im.add_command(label=GetText("Exit"), command=w.destroy)
 
+    # Refresh main window buttons
     MinimizeB.config(text=GetText("Minimize"))
     MaximizeB.config(text=GetText("Maximize"))
     NormalB.config(text=GetText("Normalize"))
@@ -119,78 +127,88 @@ def SetLang(targetlang):
     AboutB.config(text=GetText("About..."))
     ExitB.config(text=GetText("Exit"))
 
+    # Refresh main window title
     w.title("ClassIn Mover v" + __version__ + (GetText(" - without Admin") if NoAdmin else ""))
 
 
+# Callback function of EnumWindow
 @ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.wintypes.HWND, ctypes.wintypes.LPARAM)
 def EnumWindowCallback(hwnd, lParam):
     global ClassInHwnd, ClassInPID, ClassInTitle
-    textlen = ctypes.windll.user32.GetWindowTextLengthW(hwnd)
+
+    # Get window title
+    textlen = user32.GetWindowTextLengthW(hwnd)
     text = ctypes.create_unicode_buffer("", textlen + 20)
-    ctypes.windll.user32.GetWindowTextW(hwnd, text, textlen + 20)
+    user32.GetWindowTextW(hwnd, text, textlen + 20)
     Caption = text.value
     if not Caption.startswith("Classroom_"):
         return 1
+
+    # Get program PID
     pid = struct.pack("l", (0))
-    ctypes.windll.user32.GetWindowThreadProcessId(hwnd, pid)
+    user32.GetWindowThreadProcessId(hwnd, pid)
     pid = struct.unpack("l", pid)[0]
     processHandle = ctypes.windll.kernel32.OpenProcess(0x0410, 0, pid)
     if processHandle == 0:
         return 1
+
+    # Get program executable file name
     name_buffer = ctypes.create_unicode_buffer("", 260)
     ctypes.windll.psapi.GetModuleFileNameExW(processHandle, 0, name_buffer, 260)
     ctypes.windll.kernel32.CloseHandle(processHandle)
     if not name_buffer.value.lower().endswith("classin.exe"):
         return 1
+
+    # Found target window
     ClassInHwnd.append(hwnd)
     ClassInTitle.append(Caption)
     ClassInPID.append(pid)
     return 1
 
 
+# Enum windows
 def GetClassInHwnd():
     global ClassInHwnd, ClassInPID, ClassInTitle
     ClassInHwnd = []
     ClassInTitle = []
     ClassInPID = []
-    ctypes.windll.user32.EnumWindows(EnumWindowCallback, 0)
+    user32.EnumWindows(EnumWindowCallback, 0)
     return list(zip(ClassInPID, ClassInHwnd, ClassInTitle))
 
 
+# Processing thread, avoid blocking message loop thread (which will cause "Not responding")
 def ScanWindow():
     global run
-    last = set()
-    count = 0
+    last = set()  # Check whether a window is newly opened
     while run:
         try:
             st = time.time()
-            if not w.focus_get():
+            if not w.focus_get():  # Avoid being covered by other window
                 I.attributes("-topmost", 1)
                 w.attributes("-topmost", 1)
-            count += 1
             CIHwnd = GetClassInHwnd()
-            if len(CIHwnd) != 0:
+            if len(CIHwnd) != 0:  # Found window
                 newvalues = []
                 newset = set(i[1] for i in CIHwnd)
                 if newset != last:
                     for i in CIHwnd:
                         newvalues.append(GetText("%d (Title=%s PID=%d)") % (i[1], i[2], i[0]))
                         if (i[1] not in last) and DoAutoPatch.get():
-                            w.after(8500, lambda: AutoPatch(hwnd=i[1]))
-                    WindowSelector.config(values=newvalues)
+                            w.after(8500, lambda: AutoPatch(hwnd=i[1]))  # Auto patch new window
+                    WindowSelector.config(values=newvalues)  # Refresh drop-down list
                     last = newset
-                    if not WindowSelector.get() in newvalues:
+                    if not WindowSelector.get() in newvalues:  # Ensures selected window exists
                         WindowSelector.set(newvalues[0])
-            elif len(WindowSelector.get()) != 0:
+            elif len(WindowSelector.get()) != 0:  # Didn't find window but has selected one
                 WindowSelector.set("")
                 WindowSelector.config(values=[])
-            wait = math.ceil(st) - time.time()
+            wait = math.ceil(st) - time.time()  # Wait for the start of next second
             time.sleep(wait if wait >= 0 else 0)
         except:
-            import traceback
             logging.critical(traceback.format_exc())
 
 
+# Moving floating icon started
 def MouseDownI(event):
     global imx, imy, imxr, imyr
     imx = event.x
@@ -199,38 +217,44 @@ def MouseDownI(event):
     imyr = event.y_root
 
 
+# Move floating icon
 def MouseMoveI(event):
     I.geometry(f"+{event.x_root - imx}+{event.y_root - imy}")
 
 
+# Click on floating icon
 def MouseUpI(event):
-    if imxr == event.x_root and imyr == event.y_root:
+    if imxr == event.x_root and imyr == event.y_root:  # Just click, not drag
         SwitchController()
 
 
+# Resizing or moving ClassIn window started
 def MouseDownM(event):
     global mx, my
     mx = event.x_root
     my = event.y_root
 
 
+# Move ClassIn window
 def MouseMoveM(event):
     MoveWindow(x=event.x_root - mx, y=event.y_root - my, relative=True)
-    ctypes.windll.user32.SetCursorPos(mx, my)
+    user32.SetCursorPos(mx, my)
 
 
+# Resize ClassIn window
 def MouseMoveR(event):
     MoveWindow(cx=event.x_root - mx, cy=event.y_root - my, relative=True)
-    ctypes.windll.user32.SetCursorPos(mx, my)
+    user32.SetCursorPos(mx, my)
 
 
+# Move ClassIn window to center
 def Center():
     if not (hwnd := GetWindow()):
         return
     rect = struct.pack("llll", *([0] * 4))
-    ctypes.windll.user32.GetWindowRect(hwnd, rect)
+    user32.GetWindowRect(hwnd, rect)
     rect = struct.unpack("llll", rect)
-    ctypes.windll.user32.MoveWindow(
+    user32.MoveWindow(
         hwnd,
         (w.winfo_screenwidth() - rect[2] + rect[0]) // 2,
         (w.winfo_screenheight() - rect[3] + rect[1]) // 2,
@@ -240,6 +264,7 @@ def Center():
     )
 
 
+# Show or hide main window
 def SwitchController():
     global root_shown
     if root_shown:
@@ -250,26 +275,29 @@ def SwitchController():
         root_shown = True
 
 
+# Get selected window HWND
 def GetWindow():
     s = WindowSelector.get()
     if len(s) == 0:
         return 0
-    return int(s.split(" ", 1)[0])
+    return int(re.match("\d+", s).group())
 
 
+# Main function of moving window
 def MoveWindow(hwnd=None, sw=None, InsertAfter=None, x=None, y=None, cx=None, cy=None, relative=False):
     if hwnd is None:
         hwnd = GetWindow()
     if not hwnd:
         return
+    # Maximize, minimize, ...
     if sw is not None:
-        ctypes.windll.user32.ShowWindow(hwnd, sw)
-    if not relative:
+        user32.ShowWindow(hwnd, sw)
+    if not relative:  # Absolute position or size
         if InsertAfter is not None:
             rect = struct.pack("llll", *([0] * 4))
-            ctypes.windll.user32.GetWindowRect(hwnd, rect)
+            user32.GetWindowRect(hwnd, rect)
             rect = struct.unpack("llll", rect)
-            ctypes.windll.user32.SetWindowPos(
+            user32.SetWindowPos(
                 hwnd,
                 ctypes.wintypes.HWND(InsertAfter),
                 rect[0] if x is None else x,
@@ -282,9 +310,9 @@ def MoveWindow(hwnd=None, sw=None, InsertAfter=None, x=None, y=None, cx=None, cy
             w.attributes("-topmost", 1)
         else:
             rect = struct.pack("llll", *([0] * 4))
-            ctypes.windll.user32.GetWindowRect(hwnd, rect)
+            user32.GetWindowRect(hwnd, rect)
             rect = struct.unpack("llll", rect)
-            ctypes.windll.user32.MoveWindow(
+            user32.MoveWindow(
                 hwnd,
                 rect[0] if x is None else x,
                 rect[1] if y is None else y,
@@ -295,9 +323,9 @@ def MoveWindow(hwnd=None, sw=None, InsertAfter=None, x=None, y=None, cx=None, cy
     else:
         if InsertAfter is not None:
             rect = struct.pack("llll", *([0] * 4))
-            ctypes.windll.user32.GetWindowRect(hwnd, rect)
+            user32.GetWindowRect(hwnd, rect)
             rect = struct.unpack("llll", rect)
-            ctypes.windll.user32.SetWindowPos(
+            user32.SetWindowPos(
                 hwnd,
                 ctypes.wintypes.HWND(InsertAfter),
                 rect[0] + (0 if x is None else x),
@@ -310,9 +338,9 @@ def MoveWindow(hwnd=None, sw=None, InsertAfter=None, x=None, y=None, cx=None, cy
             w.attributes("-topmost", 1)
         else:
             rect = struct.pack("llll", *([0] * 4))
-            ctypes.windll.user32.GetWindowRect(hwnd, rect)
+            user32.GetWindowRect(hwnd, rect)
             rect = struct.unpack("llll", rect)
-            ctypes.windll.user32.MoveWindow(
+            user32.MoveWindow(
                 hwnd,
                 rect[0] + (0 if x is None else x),
                 rect[1] + (0 if y is None else y),
@@ -322,13 +350,14 @@ def MoveWindow(hwnd=None, sw=None, InsertAfter=None, x=None, y=None, cx=None, cy
             )
 
 
+# Auto patch: minimize, maximize, move to client area
 def AutoPatch(hwnd=None):
     if hwnd is None:
         hwnd = GetWindow()
     MoveWindow(hwnd=hwnd, sw=6, InsertAfter=-2)
     w.after(100, lambda: MoveWindow(hwnd=hwnd, sw=3))
     clientarea = struct.pack("llll", *((0,) * 4))
-    if ctypes.windll.user32.SystemParametersInfoW(0x30, 0, clientarea, 0):
+    if user32.SystemParametersInfoW(0x30, 0, clientarea, 0):
         clientarea = struct.unpack("llll", clientarea)
         w.after(
             200,
@@ -349,6 +378,7 @@ def startCheckUpdate():
         UpdateThread.start()
 
 
+# Bytes to str
 def HSize(size):
     s = size
     t = 0
@@ -415,6 +445,7 @@ def DownloadNew(urls, filename, wnd):
     downloading = False
 
 
+# Check for updates
 def CheckUpdate(ShowEvenLatest=False):
     try:
         res = urllib.request.urlopen("https://carlgao4.github.io/ClassIn-Mover/update.json")
@@ -429,15 +460,15 @@ def CheckUpdate(ShowEvenLatest=False):
         U.resizable(False, False)
         U.iconbitmap(str(pathlib.Path(__file__).parent / "ClassIn_Mover.ico"))
         U.grab_set()
-        style = ctypes.windll.user32.GetWindowLongW(int(U.frame(), 16), -16)
+        style = user32.GetWindowLongW(int(U.frame(), 16), -16)
         style &= ~0x00020000
-        ctypes.windll.user32.SetWindowLongW(int(U.frame(), 16), -16, style)
+        user32.SetWindowLongW(int(U.frame(), 16), -16, style)
 
         UpdateInfo = tkinter.Label(
             U,
             justify=tkinter.LEFT,
             text=GetText("New version %s detected\nFeatures:\n%s\n\nWe suggest you to update now. ")
-                 % (newversion["version"], newversion["feature"]),
+            % (newversion["version"], newversion["feature"]),
         )
         UpdateInfo.pack(fill=tkinter.X, anchor="nw", padx=(40, 40), pady=(40, 20))
         UF = tkinter.Frame(U)
@@ -460,18 +491,17 @@ def CheckUpdate(ShowEvenLatest=False):
         )
 
 
-def ShowText(
-        master, text="", title="", showscr=True, model=True, width=80, height=15, font=("", 12), justify=tkinter.LEFT
-):
+# Open a window and show some text
+def ShowText(master, text="", title="", showscr=True, model=True, width=80, height=15, font=("", 12)):
     TL = tkinter.Toplevel(master)
     TL.attributes("-topmost", True)
     TL.resizable(False, False)
     Text = tkinter.Text(TL, width=width, height=height, font=font, wrap="word")
     TL.title(title)
     TL.iconbitmap(str(pathlib.Path(__file__).parent / "ClassIn_Mover.ico"))
-    style = ctypes.windll.user32.GetWindowLongW(int(TL.frame(), 16), -16)
+    style = user32.GetWindowLongW(int(TL.frame(), 16), -16)
     style &= ~0x00020000
-    ctypes.windll.user32.SetWindowLongW(int(TL.frame(), 16), -16, style)
+    user32.SetWindowLongW(int(TL.frame(), 16), -16, style)
     if showscr:
         scr = tkinter.ttk.Scrollbar(TL, orient=tkinter.VERTICAL)
         scr.config(command=Text.yview)
@@ -486,6 +516,7 @@ def ShowText(
 
 
 if __name__ == "__main__":
+    # Logging
     logfolder = pathlib.Path.home() / "AppData" / "Local" / "ClassIn-Mover" / "log"
     logfolder.mkdir(parents=True, exist_ok=True)
     log = open(
@@ -505,6 +536,7 @@ if __name__ == "__main__":
     logging.info("Python version: " + sys.version)
     logging.info(platform.platform())
 
+    # Main window
     w = tkinter.Tk()
     w.resizable(False, False)
     w.title("ClassIn Mover v" + __version__ + (GetText(" - without Admin") if NoAdmin else ""))
@@ -513,9 +545,9 @@ if __name__ == "__main__":
     w.protocol("WM_DELETE_WINDOW", SwitchController)
     root_shown = False
 
-    style = ctypes.windll.user32.GetWindowLongW(int(w.frame(), 16), -16)
+    style = user32.GetWindowLongW(int(w.frame(), 16), -16)
     style &= ~0x00020000
-    ctypes.windll.user32.SetWindowLongW(int(w.frame(), 16), -16, style)
+    user32.SetWindowLongW(int(w.frame(), 16), -16, style)
 
     w.withdraw()
 
@@ -533,7 +565,7 @@ if __name__ == "__main__":
     SwitchB = tkinter.ttk.Button(
         w,
         text=GetText("Switch To"),
-        command=lambda: (ctypes.windll.user32.SetForegroundWindow(GetWindow()) if GetWindow() else None),
+        command=lambda: (user32.SetForegroundWindow(GetWindow()) if GetWindow() else None),
     )
     AutoB = tkinter.ttk.Button(w, text=GetText("Auto Patch"), command=AutoPatch)
     DragF = tkinter.LabelFrame(
@@ -581,16 +613,21 @@ if __name__ == "__main__":
     AboutB.grid(row=4, column=2, padx=(5, 5), pady=(5, 20))
     ExitB.grid(row=4, column=3, padx=(5, 20), pady=(5, 20))
 
+    # Binding mouse events
     DragF.bind("<ButtonPress-1>", MouseDownM)
     DragF.bind("<B1-Motion>", MouseMoveM)
     DragF.bind("<Double-Button-1>", lambda _: Center())
     MoveF.bind("<ButtonPress-1>", MouseDownM)
     MoveF.bind("<B1-Motion>", MouseMoveR)
     MoveF.bind("<Double-Button-1>", lambda _: MoveWindow(cx=w.winfo_screenwidth(), cy=w.winfo_screenheight()))
+
+    # Floating icon
     I = tkinter.Toplevel()
     I.overrideredirect(True)
     I.geometry("48x48+96+96")
     I.resizable(False, False)
+
+    # Right-click menu
     DoAutoPatch = tkinter.BooleanVar(I, value=True)
     im = tkinter.Menu(I, tearoff=False)
     lm = tkinter.Menu(im, tearoff=False)
@@ -614,10 +651,13 @@ if __name__ == "__main__":
     im.add_command(label=GetText("Check updates"), command=startCheckUpdate)
     im.add_command(label=GetText("Exit"), command=w.destroy)
 
+    # Icon
     img = pickle.loads(zlib.decompress(base64.b85decode(icon)))
     imgTk = PIL.ImageTk.PhotoImage(img)
     il = tkinter.Label(I, bd=0, image=imgTk)
     il.place(x=0, y=0)
+
+    # Attributes
     I.protocol("WM_DELETE_WINDOW", lambda: 0)
     I.attributes("-topmost", 1)
     I.attributes("-alpha", 0.7)
